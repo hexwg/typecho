b45b81ccdf62f2350b9efe16107408495dd0bc38
cqit@live.cn <cqit@live.cn>
2026-03-01 19:21:12 +0800

    增加数据库配置

---
+++ config.inc.php
@@ -0,0 +1,31 @@
+<?php
+// site root path
+define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
+
+// plugin directory (relative path)
+define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
+
+// theme directory (relative path)
+define('__TYPECHO_THEME_DIR__', '/usr/themes');
+
+// admin directory (relative path)
+define('__TYPECHO_ADMIN_DIR__', '/admin/');
+
+// register autoload
+require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
+
+// init
+\Typecho\Common::init();
+
+// config db
+$db = new \Typecho\Db('Pdo_Pgsql', 'typecho_');
+$db->addServer(array (
+  'host' => 'ep-crimson-recipe-ahohf9v2-pooler.c-3.us-east-1.aws.neon.tech',
+  'port' => 5432,
+  'user' => 'neondb_owner',
+  'password' => 'npg_ZG9zyEnRUKg4',
+  'charset' => 'utf8',
+  'database' => 'neondb',
+  'sslVerify' => true,
+), \Typecho\Db::READ | \Typecho\Db::WRITE);
+\Typecho\Db::set($db);
